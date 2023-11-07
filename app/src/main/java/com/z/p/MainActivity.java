9812b28c56f9dff8d82a72372906de6571c48123
takieddine12 <algeriahero22@gmail.com>
2023-11-07 15:37:13 +0100

    new changes

--- app/src/main/java/com/z/p/MainActivity.java
+++ app/src/main/java/com/z/p/MainActivity.java
@@ -6,11 +6,14 @@ import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Toast;
 
 import com.google.android.material.bottomsheet.BottomSheetBehavior;
+import com.google.android.material.tabs.TabLayout;
 
 public class MainActivity extends AppCompatActivity {
     private BottomSheetBehavior<?> mBottomSheetBehavior;
+    private TabLayout tabLayout;
     private RecyclerView rv;
     private SimpleAdapter simpleAdapter;
 
@@ -20,17 +23,42 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         rv = findViewById(R.id.rv);
+        tabLayout = findViewById(R.id.tabLayout);
 
         View bottomSheet = findViewById(R.id.bottom_sheet);
         mBottomSheetBehavior = BottomSheetBehavior.from(bottomSheet);
+        bottomSheet.setNestedScrollingEnabled(false);
+        setUpTabs();
         setUpRV();
     }
 
+    private void setUpTabs(){
+        tabLayout.addTab(tabLayout.newTab().setText("Tab1"));
+        tabLayout.addTab(tabLayout.newTab().setText("Tab2"));
+        tabLayout.addTab(tabLayout.newTab().setText("Tab3"));
+        tabLayout.addTab(tabLayout.newTab().setText("Tab4"));
+        tabLayout.addOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
+            @Override
+            public void onTabSelected(TabLayout.Tab tab) {
+                Toast.makeText(MainActivity.this,"You selected " + tab.getPosition(),Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onTabUnselected(TabLayout.Tab tab) {
+
+            }
+
+            @Override
+            public void onTabReselected(TabLayout.Tab tab) {
+
+            }
+        });
+    }
     private void setUpRV() {
         GridLayoutManager gridLayoutManager = new GridLayoutManager(this, 3);
         gridLayoutManager.setAutoMeasureEnabled(true);
         rv.setLayoutManager(gridLayoutManager);
-        rv.setNestedScrollingEnabled(false);
+        rv.setNestedScrollingEnabled(true);
         simpleAdapter = new SimpleAdapter(Extras.getImagesList());
         rv.setAdapter(simpleAdapter);
     }
